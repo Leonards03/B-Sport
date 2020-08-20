29d25cf74cbc10f30caf98f7040629e006426461
Leonards3 <edwin.leonards03@gmail.com>
2020-08-20 15:25:56 +0700

    fixes pushManager registration on first time

--- src/script/services/services.js
+++ src/script/services/services.js
@@ -24,26 +24,27 @@ function requestPermission(){
             return;
         }
         console.log("Notification allowed");
-
-        if (("PushManager" in window)) {
-            navigator.serviceWorker.getRegistration().then(registration => {
-                registration.pushManager.subscribe({
-                        userVisibleOnly: true,
-                        applicationServerKey: urlBase64ToUint8Array("BJrdR3J_ruGcYlmtg1CVuUFJpy_PHGtg4pL2dmnEMOKnsoEX8dgWfYFo6RVDRa1-Rhi0f_kT6trJQjnU5qWFAgA")
-                    })
-                    .then(subscribe => {
-                        let keys = {
-                            "p256dh": btoa(String.fromCharCode.apply(
-                                    null, new Uint8Array(subscribe.getKey("p256dh")))) ,
-                            "auth": btoa(String.fromCharCode.apply(
-                                    null, new Uint8Array(subscribe.getKey("auth"))))
-                        }
-                        console.log("Subscribe successful with endpoint: ", subscribe.endpoint);
-                        console.log("Subscribe successful with p256dh key: ", keys["p256dh"]);
-                        console.log("Subscribe successful with auth key: ", keys["auth"]);
-                    }).catch(error)
-            });
-        }
+        navigator.serviceWorker.ready.then(() => {
+            if (("PushManager" in window)) {
+                navigator.serviceWorker.getRegistration().then(registration => {
+                    registration.pushManager.subscribe({
+                            userVisibleOnly: true,
+                            applicationServerKey: urlBase64ToUint8Array("BJrdR3J_ruGcYlmtg1CVuUFJpy_PHGtg4pL2dmnEMOKnsoEX8dgWfYFo6RVDRa1-Rhi0f_kT6trJQjnU5qWFAgA")
+                        })
+                        .then(subscribe => {
+                            let keys = {
+                                "p256dh": btoa(String.fromCharCode.apply(
+                                        null, new Uint8Array(subscribe.getKey("p256dh")))) ,
+                                "auth": btoa(String.fromCharCode.apply(
+                                        null, new Uint8Array(subscribe.getKey("auth"))))
+                            }
+                            console.log("Subscribe successful with endpoint: ", subscribe.endpoint);
+                            console.log("Subscribe successful with p256dh key: ", keys["p256dh"]);
+                            console.log("Subscribe successful with auth key: ", keys["auth"]);
+                        }).catch(error)
+                });
+            }
+        });
     })
 }
 
